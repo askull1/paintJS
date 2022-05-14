f1b5360e17883e2a634e2059a427aef95346f0c4
askull1 <nuclshine@gmail.com>
2022-05-14 19:41:03 +0900

    color change

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName('jsColor')
+
+canvas.width = 1000;
+canvas.height = 700;
 
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
@@ -23,6 +27,11 @@ function onMouseMove(event) {
     ctx.stroke();
   }
 }
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  console.log(color)
+}
 
 if(canvas) {
   canvas.addEventListener("mousemove",onMouseMove);
@@ -30,3 +39,7 @@ if(canvas) {
   canvas.addEventListener("mouseup",stopPainting);
   canvas.addEventListener("mouseleave",stopPainting);
 }
+
+Array.from(colors).forEach(color => 
+  color.addEventListener("click",handleColorClick)
+  );
